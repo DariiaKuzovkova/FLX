d8cfd2ac85cb22e3719c65ef600b8816cd9ae9a9
Dariia Kuzovkova <dariia.kuzovkova@gmail.com>
2019-04-14 18:33:58 +0300

    Add files via upload

---
+++ FLX_homework_21/src/app.controller.js
@@ -0,0 +1,30 @@
+import { MoviesService } from "./common/services/movies.service";
+
+export class AppController {
+  constructor(moviesService) {
+
+    this.moviesService = moviesService;
+
+    this.button = null;
+    this.user = {};
+    this.movies = [];
+  }
+
+  $onInit() {
+    this.button = "Logout";
+    this.user = {
+      email: "my@email.com",
+      password: "3%2f43#}54f[st31"
+    };
+    this.moviesService.getAllMovies().then((result) => {
+      this.movies = result.data;
+    });
+  }
+
+  logout(event) {
+    if (event.userEmail === this.user.email) {
+      console.log(">>>User has been logged out: ", { email: event.userEmail });
+    }
+  }
+}
+AppController.$inject = [MoviesService.serviceName];
---
+++ FLX_homework_21/src/common/services/movies.service.js
@@ -0,0 +1,22 @@
+ export class MoviesService {
+  constructor($http) {
+    this.$http = $http;
+    this.movies = [];
+  }
+
+   findMovieById(id) {
+    return Promise.resolve(this.movies.data.find(element => Number(element.id) === Number(id)));
+  }
+
+   getAllMovies() {
+    return this.$http({
+      method: 'GET',
+      url: 'https://gist.githubusercontent.com/dmk1111/2714c5d780a1cdc723075ec7bec38501/raw/72479f9c108f26b009afef7b02e7b2243736d1c8/movies.json'
+    }).then((result) => {
+      return this.movies = result.data;
+    });
+  }
+}
+
+MoviesService.serviceName = 'moviesService';
+MoviesService.$inject = ['$http'];
